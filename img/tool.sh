913481f4a47b1a9b54f829681823bf4c50f95617
Ronmi Ren <ronmi.ren@gmail.com>
2021-06-27 19:26:48 +0800

    update to easyrsa3

--- img/tool.sh
+++ img/tool.sh
@@ -1,38 +1,34 @@
 #!/bin/bash
 CA=/etc/openvpn/rsa
 
-cd "$CA"
-
 function init_ca {
+    mkdir -p "${CA}/keys"
+    chmod 700 "${CA}/keys"
     cp -ra /usr/share/easy-rsa/* .
-    cp openssl-1.0.0.cnf openssl.cnf
+    cp openssl-easyrsa.cnf openssl.cnf
+    cp vars.example vars
     echo ""
     echo "You can now open another terminal and edit the 'conf/rsa/vars' file to fit your need."
     echo "This step is optional, just skip it if you have no idea what it is."
     echo ""
     echo "After you are prepared, press Enter to continue..."
     read a
-    source ./vars
 
-    ./clean-all
+    echo yes | ./easyrsa init-pki
+    ./easyrsa gen-dh
+    (echo;echo;echo;echo) | ./easyrsa build-ca nopass
 
-    # generate ca
-    ./pkitool --initca --batch
-    ./build-dh
-
-    # generate server key
-    ./pkitool --server --batch server
+    ./easyrsa build-server-full server nopass
 }
 
 function gen {
-    source ./vars
     if [[ $1 == "" ]]
     then
 	echo "You must provide client key-file name"
 	return 1
     fi
 
-    ./pkitool --batch "$1"
+    ./easyrsa build-client-full "$1" nopass
 }
 
 function ovpn {
@@ -58,14 +54,14 @@ function ovpn {
     echo "ns-cert-type server"
     grep -E '^comp' "$tmpf"
     echo '<ca>'
-    cat "${CA}/keys/ca.crt"
+    cat "${CA}/pki/issued/server.crt"
     echo '</ca>'
     echo '<cert>'
-    lineno=$(grep -nE '^-----BEGIN CERTIFICATE-----$' "${CA}/keys/${user}.crt"|cut -d ':' -f 1)
-    cat "${CA}/keys/${user}.crt" | tail -n "+${lineno}"
+    lineno=$(grep -nE '^-----BEGIN CERTIFICATE-----$' "${CA}/pki/issued/${user}.crt"|cut -d ':' -f 1)
+    cat "${CA}/pki/issued/${user}.crt" | tail -n "+${lineno}"
     echo '</cert>'
     echo '<key>'
-    cat "${CA}/keys/${user}.key"
+    cat "${CA}/pki/private/${user}.key"
     echo '</key>'
 }
 
@@ -91,7 +87,7 @@ function check {
 
     cd "$CA"
 
-    if [[ ! -f "${CA}/pkitool" || ! -f "${CA}/vars" || ! -f "${CA}/openssl.cnf" ]]
+    if [[ ! -f "${CA}/easyrsa" || ! -f "${CA}/vars" || ! -f "${CA}/openssl.cnf" ]]
     then
 	init_ca
     fi
@@ -110,7 +106,7 @@ case "$1" in
     ovpn)
 	check
 	cd "$CA"
-	if [[ ! -f "keys/${3}.key" || ! -f "keys/${3}.crt" ]]
+	if [[ ! -f "pki/private/${3}.key" || ! -f "pki/issued/${3}.crt" ]]
 	then
 	    gen "$3"
 	    echo ""
